ab63ab7a8426e5e4f065dd4c53f385258054b44b
DatAKAjezz <jezzthedead@gmail.com>
2024-07-07 06:54:47 +0700

    update 7/7/2024

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const words = [
-    { word: "san ho", hint: "Lá phổi của Trái Đất" , revealsNumber : 0},
-    { word: "song than", hint: "Một thảm họa thiên nhiên",revealsNumber : 0 },
-    { word: "tuyet lo", hint: "Một trong những hậu quả của hiện tượng nóng lên toàn cầu", revealsNumber : 0},
-    { word: "sao bien", hint: "Một sinh vật sống dưới nước có khả năng chữa lành siêu việt", revealsNumber : 0 },
-    { word: "ca thon bon", hint: "Người ta hay gọi nó là chiếc lưỡi biết bơi", revealsNumber : 0 },
-    { word: "thuy trieu do", hint : "Hiện tượng tự nhiên ở biển có màu sắc đặc trưng, thường gây hại cho sinh vật biển", revealsNumber : 0}
+    { word: "san hô", hint: "Lá phổi của Trái Đất" , revealsNumber : 0},
+    { word: "sóng thần", hint: "Một thảm họa thiên nhiên",revealsNumber : 0 },
+    { word: "tuyết lở", hint: "Một trong những hậu quả của hiện tượng nóng lên toàn cầu", revealsNumber : 0},
+    { word: "sao biển", hint: "Một sinh vật sống dưới nước có khả năng chữa lành siêu việt", revealsNumber : 0 },
+    { word: "cá thờn bơn", hint: "Người ta hay gọi nó là chiếc lưỡi biết bơi", revealsNumber : 0 },
+    { word: "thủy triều đỏ", hint : "Hiện tượng tự nhiên ở biển có màu sắc đặc trưng, thường gây hại cho sinh vật biển", revealsNumber : 0}
 ];
 
 words.map((val) => {
@@ -48,14 +48,15 @@ function render() {
                 }
                 letterBoxes.appendChild(box);
             }
-        }else{     
+        }else{ 
+            let tmpString = removeDiacritics(wordObj.word);    
             for (let i = 0; i < wordObj.word.length; i++) {
                 const box = document.createElement("div");
                 box.className = "letter-box";
                 box.onclick = () => revealLetter(wordIndex, i);
                 
                 if (revealedLetters[wordIndex][i]) {
-                    box.textContent = wordObj.word[i];
+                    box.textContent = tmpString[i];
                     box.classList.add("revealed");
                 }
                 letterBoxes.appendChild(box);
@@ -68,8 +69,16 @@ function render() {
         buttons.className = "buttons";
 
         const revealButton = document.createElement("button");
-        revealButton.textContent = "Hiện tất cả";
-        revealButton.onclick = () => revealWord(wordIndex);
+
+        if (countLetters !== wordObj.word.length){
+            revealButton.textContent = "Hiện tất cả";
+            revealButton.onclick = () => revealWord(wordIndex);
+
+        }else{
+            revealButton.textContent = "Ẩn tất cả..";
+            revealButton.onclick = () => hideWord(wordIndex);
+        }
+
         buttons.appendChild(revealButton);
 
         const hintButton = document.createElement("button");
@@ -94,9 +103,22 @@ function revealWord(wordIndex) {
     render();
 }
 
+function hideWord(wordIndex) {
+    revealedLetters[wordIndex] = revealedLetters[wordIndex].map(() => false);  
+    render();
+}
+
 function showHint(wordIndex) {
     const hintContainer = document.getElementById("hintContainer");
     hintContainer.textContent = words[wordIndex].hint;
 }
+
+function removeDiacritics(str) {
+    return str.normalize('NFD')
+              .replace(/[\u0300-\u036f]/g, '')
+              .replace(/đ/g, 'd').replace(/Đ/g, 'D');
+}
+
+console.log(removeDiacritics("Xin chào tôi là người Việt Nam"));
 
 window.onload = initializeGame;
